dc7de7b5c30e62afe0f50aca5383466905b28283
maxschnupp <maxschnupp@gmail.com>
2021-09-22 10:01:14 +0000

    merge

--- app/constants.py
+++ app/constants.py
@@ -2,8 +2,13 @@ TRAIN_TFRECORD = 'assets/data/train.tfrecord'
 TRAIN_TFRECORD_FILEPATTERN = TRAIN_TFRECORD + '*'
 STATS_PATH = "assets/statistics"
 SAVE_DIR = "assets/models/ddsp-solo-instrument"
+<<<<<<< HEAD
 GIN_BUCKET_NAME = "ddsp-gin-files"
 AUDIO_BUCKET_NAME = "ddsp-audio-renders"
 TAG = "flugelhorn"
 AUDIO_INPUT = './assets/audioIn'
 AUDIO_OUTPUT = './assets/audioOut'
+=======
+BUCKET_NAME = "ddsp-gin-files"
+TAG = "flugelhorn"
+>>>>>>> 7ba97f2... flugelHorn audio
