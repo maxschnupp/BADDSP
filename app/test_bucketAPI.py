b422f1310a7b4a3ea6d5fc3ff1c25075fa26e67f
maxschnupp <maxschnupp@gmail.com>
2021-10-19 11:38:09 +0000

    fix tests

--- app/test_bucketAPI.py
+++ app/test_bucketAPI.py
@@ -3,7 +3,7 @@ import unittest
 import unittest.mock as mock
 class TestBucketAPI(unittest.TestCase):
     def test_upload_to_gin_bucket(self):
-            bucketAPI.upload_to_gin_bucket("vibraphone-ckpt-1800.data-00000-of-00001", "testing")
+            bucketAPI.upload_to_gin_bucket("test.txt", "testing")
 
     def test_upload_to_audio_bucket(self):
             bucketAPI.upload_to_audio_bucket("test_audio.wav", "testing")
